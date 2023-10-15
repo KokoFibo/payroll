3811015ac49e4aa79b0a2ac9819d88bd65501c35
Anton <kokonacci@gmail.com>
2023-10-15 12:19:14 +0700

    percantik dashboard mobile

--- resources/views/dashboardMobile.blade.php
+++ resources/views/dashboardMobile.blade.php
@@ -1,1 +1,48 @@
-<h1>Silakan Akses melalui PC</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Under Construction</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background: linear-gradient(135deg, #3494E6, #EC6EAD),
+                url('under_construction.jpg');
+            /* Add your image URL here */
+            background-size: cover;
+            background-position: center;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+
+        .container {
+            text-align: center;
+            background-color: rgba(255, 255, 255, 0.8);
+            /* Semi-transparent white background */
+            border-radius: 10px;
+            padding: 20px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            color: #333;
+            font-size: 24px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Silakan akses via PC</h1>
+    </div>
+</body>
+
+</html>
