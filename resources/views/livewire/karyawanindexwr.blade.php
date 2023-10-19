ddf81b84fff840757c4bb0c1672cd0c984b4725c
Anton <kokonacci@gmail.com>
2023-10-19 21:51:49 +0700

    betulin tabel done

--- resources/views/livewire/karyawanindexwr.blade.php
+++ resources/views/livewire/karyawanindexwr.blade.php
@@ -34,10 +34,11 @@
                     <table class="table table-xl   table-hover mb-2">
                         <thead>
                             <tr>
-                                <th wire:click="sortColumnName('nama')">Nama <i class="fa-solid fa-sort"></i></th>
+                                <th></th>
                                 <th wire:click="sortColumnName('id_karyawan')">Id Karyawan <i
                                         class="fa-solid fa-sort"></i>
                                 </th>
+                                <th wire:click="sortColumnName('nama')">Nama <i class="fa-solid fa-sort"></i></th>
                                 <th wire:click="sortColumnName('branch')">Branch <i class="fa-solid fa-sort"></i></th>
                                 <th wire:click="sortColumnName('departemen')">Departemen <i
                                         class="fa-solid fa-sort"></i>
@@ -46,51 +47,39 @@
                                 <th wire:click="sortColumnName('level_jabatan')">Level Jabatan <i
                                         class="fa-solid fa-sort"></i>
                                 </th>
-                                <th></th>
+
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($datas as $data)
                                 <tr>
-                                    <td>{{ $data->nama }}</td>
+                                    <td>
+                                        <div class="text-start">
+                                            <a href="/karyawanupdate/{{ $data->id }}"><button
+                                                    class="btn btn-success btn-sm"><i
+                                                        class="fa-regular fa-pen-to-square"></i></button></a>
+
+                                            @if (Auth::user()->role == 3 || Auth::user()->role == 4)
+                                                <button wire:click="confirmDelete({{ $data->id }})"
+                                                    class="btn btn-danger btn-sm"><i
+                                                        class="fa-solid fa-trash-can"></i></button>
+                                            @endif
+                                        </div>
+                                    </td>
                                     <td>{{ $data->id_karyawan }}</td>
+                                    <td>{{ $data->nama }}</td>
                                     <td>{{ $data->branch }}</td>
                                     <td>{{ $data->departemen }}</td>
                                     <td>{{ $data->jabatan }}</td>
                                     <td>{{ $data->level_jabatan }}</td>
-                                    <td class="btn-group mx-auto gap-2">
-                                        <a href="/karyawanupdate/{{ $data->id }}"><button
-                                                class="btn btn-warning btn-sm"><i
-                                                    class="fa-regular fa-pen-to-square"></i></button></a>
-
-                                        @if (Auth::user()->role == 3 || Auth::user()->role == 4)
-                                            <button wire:click="confirmDelete({{ $data->id }})"
-                                                class="btn btn-danger btn-sm"><i
-                                                    class="fa-solid fa-trash-can"></i></button>
-                                            {{-- <form method="post"
-                                                action="{{ route('karyawan.destroy', ['id' => $data->id]) }}">
-                                                @csrf
-                                                @method('delete')
 
 
-
-                                                <a href="{{ route('karyawan.destroy', ['id' => $data->id]) }}"><button
-                                                        type="submit" class="btn btn-danger btn-sm confirm-delete"><i
-                                                            class="fa-solid fa-trash-can"></i></button></a>
-                                            </form> --}}
-                                        @endif
-
-
-
-                                    </td>
                                 </tr>
                             @endforeach
                         </tbody>
                     </table>
+                    {{ $datas->links() }}
                 </div>
-
-                {{ $datas->links() }}
-
             </div>
         </div>
     </div>
