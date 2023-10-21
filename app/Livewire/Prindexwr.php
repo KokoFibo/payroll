58f98d27325d26da52fe028b360c363a001efa59
Anton <kokonacci@gmail.com>
2023-10-21 10:40:41 +0700

    2nd hardest part is done

--- app/Livewire/Prindexwr.php
+++ app/Livewire/Prindexwr.php
@@ -44,25 +44,30 @@ class Prindexwr extends Component
 
         $filteredData = Jamkerjaid::get();
         foreach ($filteredData as $data) {
+            $jumlah_menit_lembur = null;
+            $jumlah_jam_terlambat = null;
+            $jumlah_menit_lembur = null;
+            $total_late_1 = null;
+            $total_late_2 = null;
+            $total_late_3 = null;
+            $total_late_4 = null;
+            $total_late_5 = null;
+            $total_late = null;
             $dataId = Yfrekappresensi::where('user_id', $data->user_id)->get();
             if (!$dataId) {
                 dd('data kosong from Prindex.php', $dataId);
             } else {
-                // $jumlah_menit_lembur = 0;
                 foreach ($dataId as $dt) {
                     if ($dt->late == null) {
                         // khusus NO Late
                         $jumlah_hari_kerja = $dataId->count();
                         if ($dt->overtime_in != null) {
                             $menitLembur = hitungLembur($dt->overtime_in, $dt->overtime_out);
-                            // $jumlah_menit_lembur = $jumlah_menit_lembur + $menitLembur;
+                            $jumlah_menit_lembur = $jumlah_menit_lembur + $menitLembur;
                         }
                     } else {
                         // khusus yang late
                         $jumlah_hari_kerja = $dataId->count();
-                        // $jumlah_menit_lembur ;
-                        // $jumlah_jam_terlambat = 0;
-                        // $jumlah_telat_overtime_hours = 0;
 
                         // check keterlambatan di hari kerja non overtime
                         $late1 = checkFirstInLate($dt->first_in, $dt->shift, $dt->date);
@@ -70,40 +75,38 @@ class Prindexwr extends Component
                         $late3 = checkSecondInLate($dt->second_in, $dt->shift, $dt->first_out, $dt->date);
                         $late4 = checkSecondOutLate($dt->second_out, $dt->shift, $dt->date);
                         $late5 = checkOvertimeInLate($dt->overtime_in, $dt->shift, $dt->date);
-                        $late = $late1 + $late2 + $late3 + $late4 + $late5;
-
-                        // foreach ($dataId as $dt) {
-                        //     if ($dt->overtime_in != null) {
-                        $menitLembur = hitungLembur($dt->overtime_in, $dt->overtime_out);
-                        // $jumlah_menit_lembur = $jumlah_menit_lembur + $menitLembur;
-                        // $jumlah_jam_terlambat = $jumlah_jam_terlambat + $late;
-                        //     }
-                        // }
-                        // $jumlah_jam_terlambat = $jumlah_jam_terlambat + $late;
-                        // dd($jumlah_jam_terlambat);
+                        $total_late_1 = $total_late_1 + $late1;
+                        $total_late_2 = $total_late_2 + $late2;
+                        $total_late_3 = $total_late_3 + $late3;
+                        $total_late_4 = $total_late_4 + $late4;
+                        $total_late_5 = $total_late_5 + $late5;
+                        $total_late = $total_late_1 + $total_late_2 + $total_late_3 + $total_late_4 + $total_late_5;
+                        if ($dt->overtime_in != null) {
+                            $menitLembur = hitungLembur($dt->overtime_in, $dt->overtime_out);
+                            $jumlah_menit_lembur = $jumlah_menit_lembur + $menitLembur;
+                        }
                     }
-                    $jumlah_menit_lembur = $jumlah_menit_lembur + $menitLembur;
-
-                    $jumlah_jam_terlambat = $jumlah_jam_terlambat + $late;
                 }
-            }
 
-            $jumlah_jam_kerja = $jumlah_hari_kerja * 8;
-            // dd($jumlah_menit_lembur, $dt->user_id);
-            // dd( $dt->name);
+                $jumlah_jam_terlambat = $jumlah_jam_terlambat + $late;
+            }
+            // DATA TOTAL
+
+            $jumlah_jam_kerja = $jumlah_hari_kerja * 8 - ($total_late + $total_late_5);
+
             $data = Jamkerjaid::find($data->id);
             $data->name = $dt->name;
             $data->jumlah_jam_kerja = $jumlah_jam_kerja;
             $data->jumlah_menit_lembur = $jumlah_menit_lembur;
-            $data->jumlah_jam_terlambat = $jumlah_jam_terlambat;
-            $data->first_in_late = $late1;
-            $data->first_out_late = $late2;
-            $data->second_in_late = $late3;
-            $data->second_out_late = $late4;
-            $data->overtime_in_late = $late5;
+            $data->jumlah_jam_terlambat = $total_late;
+            $data->first_in_late = $total_late_1;
+            $data->first_out_late = $total_late_2;
+            $data->second_in_late = $total_late_3;
+            $data->second_out_late = $total_late_4;
+            $data->overtime_in_late = $total_late_5;
             $data->save();
-            $this->dispatch('success', message: 'Data Karyawan Sudah di Save');
         }
+        $this->dispatch('success', message: 'Data Karyawan Sudah di Save');
     }
 
     public function render()
