8953967a038bd16a34dd5353a91bb1d7fdfd04f0
Anton <kokonacci@gmail.com>
2023-11-09 12:17:21 +0700

    rapihin rubah id

--- app/Livewire/Rubahidwr.php
+++ app/Livewire/Rubahidwr.php
@@ -10,11 +10,33 @@ class Rubahidwr extends Component
 {
     public $idFromArr;
     public $idToArr;
+    public $from, $to;
 
     public function mount () {
        $this->idFromArr = [ 5249, 5250, 5248, 5223 ];
 
        $this->idToArr = [ 800, 900, 5105, 5301 ];
+    }
+
+    public function proses () {
+        // check if Id is exist
+        $check_karyawan = Karyawan::where('id_karyawan', $this->to)->select('id')->first();
+        $check_user = User::where('username', $this->to)->select('id')->first();
+        // dd($check_karyawan,$check_user);
+        if($check_karyawan != null || $check_user != null) {
+            $this->dispatch('error', message: 'Data Karyawan ID: '. $this->to . ' sudah ada dalam database');
+            return;
+        }
+
+        $karyawan_id = Karyawan::where('id_karyawan', $this->from)->select('id')->first();
+        $user_id = User::where('username', $this->from)->select('id')->first();
+        $karyawan = Karyawan::find($karyawan_id->id);
+        $user = User::find($user_id->id);
+        $karyawan->id_karyawan = $this->to;
+        $karyawan->save();
+        $user->username = $this->to;
+        $user->save();
+        $this->dispatch('success', message: 'Data Karyawan Sudah di Save');
     }
 
 
--- resources/views/livewire/rubahidwr.blade.php
+++ resources/views/livewire/rubahidwr.blade.php
@@ -1,10 +1,14 @@
 <div>
     rubahID
-    <button wire:click="rubah" class="btn btn-primary">Process</button>
+    {{-- <button wire:click="rubah" class="btn btn-primary">Process</button> --}}
 
-    <ul>
+    {{-- <ul>
         @for ($i = 0; $i < count($idFromArr); $i++)
             <li>{{ $idFromArr[$i] }} to {{ $idToArr[$i] }}</li>
         @endfor
-    </ul>
+    </ul> --}}
+
+    <h1>Rubah ID</h1>
+    <h3>ID dirubah dari <input wire:model="from" type="text"> ke ID <input wire:model="to" type="text"> <button
+            class="btn btn-primary" wire:click="proses">Proses</button> </h3>
 </div>
