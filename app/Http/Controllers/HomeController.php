be5d212c181cd7bcb9bdad8f40b6a6f05e44d1bf
Anton <kokonacci@gmail.com>
2023-11-10 21:43:23 +0700

    penyempurnaan tampilan user mobile

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -66,10 +66,49 @@ class HomeController extends Controller {
             $user->save();
             // return view( 'dashboardMobile1' );
             $user_id = auth()->user()->username;
-        $data = Yfrekappresensi::where('user_id', $user_id)->orderBy('date', 'desc')->get();
+
+            // $user_id = 4751;
+        $month = 11;
+        $total_hari_kerja = Yfrekappresensi::whereMonth('date', '=', 11)
+            ->distinct('date')
+            ->count();
+
+        $total_jam_kerja = 0;
+        $total_jam_lembur = 0;
+        $total_keterlambatan = 0;
+
+        $dataArr = [];
+        $data = Yfrekappresensi::where('user_id', $user_id)
+            ->orderBy('date', 'desc')
+            ->get();
+
+        foreach ($data as $d) {
+            if ($d->no_scan == null) {
+                $tgl = tgl_doang($d->date);
+                $jam_kerja = hitung_jam_kerja($d->first_in, $d->first_out, $d->second_in, $d->second_out, $d->late, $d->shift, $d->date);
+                $terlambat = late_check_jam_kerja_only($d->first_in, $d->first_out, $d->second_in, $d->second_out, $d->shift, $d->date);
+                $jam_lembur = hitungLembur($d->overtime_in, $d->overtime_out) / 60;
+                $total_jam_kerja = $total_jam_kerja + $jam_kerja;
+                $total_jam_lembur = $total_jam_lembur + $jam_lembur;
+                $total_keterlambatan = $total_keterlambatan + $terlambat;
+
+                $dataArr[] = [
+                    'tgl' => $tgl,
+                    'jam_kerja' => $jam_kerja,
+                    'terlambat' => $terlambat,
+                    'jam_lembur' => $jam_lembur,
+                ];
+            }
+        }
+
         return view('mobile')->with([
-            'data' => $data
+            'dataArr' => $dataArr,
+            'total_hari_kerja' => $total_hari_kerja,
+            'total_jam_kerja' => $total_jam_kerja,
+            'total_jam_lembur' => $total_jam_lembur,
+            'total_keterlambatan' => $total_keterlambatan,
         ]);
+
 
         }
     }
