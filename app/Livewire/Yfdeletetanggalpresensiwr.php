b32773e0e131d962c82c5484f30b7682810e7eb7
Anton <kokonacci@gmail.com>
2023-11-11 11:52:07 +0700

    rubah shift otomatis

--- app/Livewire/Yfdeletetanggalpresensiwr.php
+++ app/Livewire/Yfdeletetanggalpresensiwr.php
@@ -8,6 +8,28 @@ use App\Models\Yfrekappresensi;
 class Yfdeletetanggalpresensiwr extends Component
 {
     public $tanggal;
+    public $lokasi;
+
+    public function deleteByPabrik () {
+        if ($this->lokasi == 0) {
+        $data = Yfrekappresensi::where('placement', ['YIG','YSM'])->get();
+        dd($data, $this->lokasi);
+    }
+    if ($this->lokasi == 1) {
+        $data = Yfrekappresensi::where('placement', 'YCME')->get();
+        dd($data, $this->lokasi);
+    }
+    if ($this->lokasi == 2) {
+        $data = Yfrekappresensi::where('placement', 'YEV')->get();
+        dd($data, $this->lokasi);
+        }
+        if($data->isEmpty($data)){
+            $this->dispatch('error', message: 'Data presensi tidak ditemukan');
+        } else {
+            Yfrekappresensi::whereDate('date', $this->tanggal)->delete();
+            $this->dispatch('success', message: 'Data pada tanggal tersebut telah di hapus');
+        }
+    }
 
     public function delete () {
         $data = Yfrekappresensi::whereDate('date', $this->tanggal)->get();
