fc44b656f7f44cc03a1fda782a351b11c47f6004
Anton <kokonacci@gmail.com>
2023-12-02 14:16:46 +0700

    satpam sabtu gak lembur

--- resources/views/livewire/user-mobile.blade.php
+++ resources/views/livewire/user-mobile.blade.php
@@ -183,6 +183,10 @@
                                                 if ($d->karyawan->jabatan == 'Satpam' && is_sunday($d->date)) {
                                                     $jam_kerja = hitung_jam_kerja($d->first_in, $d->first_out, $d->second_in, $d->second_out, $d->late, $d->shift, $d->date, $d->karyawan->jabatan);
                                                 }
+
+                                                if ($d->karyawan->jabatan == 'Satpam' && is_saturday($d->date)) {
+                                                    $jam_lembur = 6;
+                                                }
                                             @endphp
                                             {{ $jam_kerja }}
                                         </p>
