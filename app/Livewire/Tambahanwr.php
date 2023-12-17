8f23ff0683b54939d09d7eeb19b0fefe8adb66ee
Anton <kokonacci@gmail.com>
2023-12-17 10:40:42 +0700

    rubah bonus dan potongan

--- app/Livewire/Tambahanwr.php
+++ app/Livewire/Tambahanwr.php
@@ -2,37 +2,55 @@
 
 namespace App\Livewire;
 
+use Carbon\Carbon;
 use Livewire\Component;
 use App\Models\Karyawan;
-use App\Models\Tambahan;
+use App\Models\Bonuspotongan;
 use Livewire\WithPagination;
-use Carbon\Carbon;
 
 class Tambahanwr extends Component
 {
     use WithPagination;
     protected $paginationTheme = 'bootstrap';
 
-    public $id, $is_edit, $id_tambahan, $modal, $search, $user_id, $nama_karyawan;
-    public $tanggal, $uang_makan,  $bonus_lain, $baju_esd, $gelas, $sandal;
+    public $id, $is_edit, $id_tambahan, $modal, $search, $user_id, $nama_karyawan, $karyawan_id;
+    public $tanggal, $uang_makan, $bonus_lain, $baju_esd, $gelas, $sandal;
     public $seragam, $sport_bra, $hijab_instan, $id_card_hilang, $masker_hijau, $potongan_lain;
+    public $year, $month;
 
-    public function mount () {
+    public function mount()
+    {
         $this->modal = false;
         $this->is_edit = false;
+        $this->year = now()->year;
+        $this->month = now()->month;
+        $this->tanggal = now()->toDateString();
     }
 
-    public function add ($id_karyawan) {
-        $data_karyawan = Karyawan::where('id_karyawan', ($id_karyawan))->first();
-        $this->id = $data_karyawan->id ;
-        $this->user_id = $id_karyawan;
-        $this->nama_karyawan = $data_karyawan->nama;
-        $this->tanggal =  date('Y-m-d');
+    public function add()
+    {
+        // $data_karyawan = Karyawan::where('id_karyawan', ($id_karyawan))->first();
+        // $this->id = $data_karyawan->id ;
+        // $this->user_id = $id_karyawan;
+        // $this->nama_karyawan = $data_karyawan->nama;
+        // $this->tanggal =  date('Y-m-d');
         $this->modal = true;
     }
 
-    public function save () {
+    public function updatedUserId()
+    {
+        try {
+            $data = Karyawan::where('id_karyawan', $this->user_id)->first();
 
+            $this->karyawan_id = $data->id;
+            $this->nama_karyawan = $data->nama;
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function save()
+    {
         if (
             $this->uang_makan == null &&
             // $this->bonus == null &&
@@ -47,20 +65,21 @@ class Tambahanwr extends Component
             $this->masker_hijau == null &&
             $this->potongan_lain == null
         ) {
-
-        $this->dispatch('error', message: 'Data tidak disimpan');
-        return;
-
+            $this->dispatch('error', message: 'Data tidak disimpan');
+            return;
         }
-        if($this->is_edit == false) {
-            $karyawan = Karyawan::find($this->id);
-            $data = new Tambahan();
-            $data->user_id = $karyawan->id_karyawan;
+        if ($this->is_edit == false) {
+            $data = new Bonuspotongan();
+            $data->user_id = $this->user_id;
+
+            $data->karyawan_id = $this->karyawan_id;
         } else {
-            $data = Tambahan::find($this->id_tambahan);
+            $data = Bonuspotongan::find($this->id_tambahan);
+            // $data->user_id = $this->user_id;
+            // $data->karyawan_id = $this->karyawan_id;
         }
+
         $data->uang_makan = $this->uang_makan;
-        // $data->bonus = $this->bonus;
         $data->bonus_lain = $this->bonus_lain;
         $data->baju_esd = $this->baju_esd;
         $data->gelas = $this->gelas;
@@ -71,12 +90,11 @@ class Tambahanwr extends Component
         $data->id_card_hilang = $this->id_card_hilang;
         $data->masker_hijau = $this->masker_hijau;
         $data->potongan_lain = $this->potongan_lain;
-        $data->tanggal = date( 'Y-m-d', strtotime( $this->tanggal ) );
+        $data->tanggal = date('Y-m-d', strtotime($this->tanggal));
         $data->save();
-        if($this->is_edit == false) {
-        $this->dispatch('success', message: 'Data sudah di Add');
-
-    } else {
+        if ($this->is_edit == false) {
+            $this->dispatch('success', message: 'Data sudah di Add');
+        } else {
             $this->dispatch('success', message: 'Data sudah di Update');
         }
         $this->is_edit = false;
@@ -84,13 +102,17 @@ class Tambahanwr extends Component
         $this->clear_data();
     }
 
-    public function cancel () {
+    public function cancel()
+    {
         $this->is_edit = false;
         $this->modal = false;
         $this->clear_data();
     }
 
-    public function clear_data () {
+    public function clear_data()
+    {
+        $this->user_id = null;
+        $this->nama_karyawan = null;
         $this->uang_makan = null;
         // $this->bonus = null;
         $this->bonus_lain = null;
@@ -103,22 +125,22 @@ class Tambahanwr extends Component
         $this->id_card_hilang = null;
         $this->masker_hijau = null;
         $this->potongan_lain = null;
-        $this->tanggal =  date( 'd M Y', strtotime( now()->toDateString() ) );
+        $this->tanggal = now()->toDateString();
     }
 
-    public function update($id) {
-        $data_karyawan = Karyawan::where('id_karyawan', $id)->first();
-        $this->user_id = $id;
-        $this->nama_karyawan = $data_karyawan->nama;
-
+    public function update($id)
+    {
         $this->is_edit = true;
         $this->modal = true;
-        $data_tambahan = Tambahan::where('user_id', $id)->first();
-        $this->id_tambahan = $data_tambahan->id ;
-        $data_tambahan = Tambahan::find($this->id_tambahan);
 
+        $this->id_tambahan = $id;
+        $data_tambahan = Bonuspotongan::find($id);
+        $data_karyawan = Karyawan::find($data_tambahan->karyawan_id);
+        $this->nama_karyawan = $data_karyawan->nama;
+
+        $this->user_id = $data_tambahan->user_id;
+        $this->karyawan_id = $data_tambahan->karyawan_id;
         $this->uang_makan = $data_tambahan->uang_makan;
-        // $this->bonus = $data_tambahan->bonus;
         $this->bonus_lain = $data_tambahan->bonus_lain;
         $this->baju_esd = $data_tambahan->baju_esd;
         $this->gelas = $data_tambahan->gelas;
@@ -133,36 +155,45 @@ class Tambahanwr extends Component
         // $this->tanggal = $data_tambahan->tanggal;
     }
 
-
-
     public function updatingSearch()
     {
         $this->resetPage();
     }
-    public function delete  ($id) {
-
-        // dd($id);
-       $data_tambahan = Tambahan::where('user_id', $id)->first();
-       $data = Tambahan::find($data_tambahan->id);
-       $data->delete();
-       $this->dispatch('success', message: 'Data sudah di Delete');
+    public function delete($id)
+    {
+        $data_tambahan = Bonuspotongan::find($id);
+        $data_tambahan->delete();
+        $this->dispatch('success', message: 'Data sudah di Delete');
     }
 
     public function render()
     {
-        $oneYearAgo = Carbon::now()->subYear();
-        $data = Karyawan::query()
-        ->select('id', 'id_karyawan', 'nama')
-        ->orderBy('id_karyawan', 'desc')
-        ->whereIn('status_karyawan', ['PKWT', 'PKWTT'])
-        ->where('tanggal_bergabung',  '>', $oneYearAgo)
-        ->when($this->search, function ($query) {
-            $query
-            ->where('nama', 'LIKE', '%' . trim($this->search) . '%')
-            ->orWhere('id_karyawan', trim($this->search));
-        })
-        ->paginate(10);
+        // $oneYearAgo = Carbon::now()->subYear();
+        // $data = Karyawan::query()
+        // ->select('id', 'id_karyawan', 'nama')
+        // ->orderBy('id_karyawan', 'desc')
+        // ->whereIn('status_karyawan', ['PKWT', 'PKWTT'])
+        // ->where('tanggal_bergabung',  '>', $oneYearAgo)
+        // ->when($this->search, function ($query) {
+        //     $query
+        //     ->where('nama', 'LIKE', '%' . trim($this->search) . '%')
+        //     ->orWhere('id_karyawan', trim($this->search));
+        // })
+        // ->paginate(10);
 
+        $month_start = Carbon::now()
+            ->startOfMonth()
+            ->subMonth(1);
+        $month_end = Carbon::now()->endOfMonth();
+
+        $data = Bonuspotongan::select(['bonuspotongans.*', 'karyawans.nama', 'karyawans.jabatan'])
+            ->join('karyawans', 'bonuspotongans.karyawan_id', '=', 'karyawans.id')
+            ->whereBetween('bonuspotongans.tanggal', [$month_start, $month_end])
+            ->where('karyawans.nama', 'LIKE', '%' . trim($this->search) . '%')
+            ->orWhere('karyawans.jabatan', 'LIKE', '%' . trim($this->search) . '%')
+            ->orWhere('bonuspotongans.user_id', trim($this->search))
+            ->orderBy('bonuspotongans.tanggal', 'desc')
+            ->paginate(10);
 
         return view('livewire.tambahanwr', compact('data'));
     }
--- app/Models/Bonuspotongan.php
+++ app/Models/Bonuspotongan.php
@@ -5,13 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Tambahan extends Model
+class Bonuspotongan extends Model
 {
     use HasFactory;
-    protected $guarded = [];
 
-    public function user () {
-        return $this->belongsTo(User::class);
+    public function karyawan () {
+        return $this->belongsTo(Karyawan::class);
     }
-
+    
 }
