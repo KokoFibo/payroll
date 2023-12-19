2ec16ad56fb7cfb0e507501db8df1db28b90e0d1
Anton <kokonacci@gmail.com>
2023-12-19 10:36:15 +0700

    dashboard tambah translation

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -62,7 +62,7 @@ class DashboardController extends Controller
         ];
 
         $jumlah_karyawan_labelArr = [
-            'Pria', 'Wanita'
+            'Pria 男', 'Wanita 女'
         ];
 
 
@@ -70,8 +70,10 @@ class DashboardController extends Controller
             $jumlah_Pabrik_1, $jumlah_Pabrik_2,  $jumlah_Kantor
         ];
         $placementLabelArr = [
-            'Pabrik 1', 'Pabrik 2',  'Kantor'
+            'Pabrik 1 工厂1', 'Pabrik 2 工厂2',  'Kantor 办公室'
         ];
+
+      
 
         $companyArr = [
             
