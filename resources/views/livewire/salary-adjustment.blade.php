1b61e26287c4199f78a43d1a5f1a4bfc2337417f
KokoFibo <kokonacci@gmail.com>
2023-12-24 10:50:43 +0700

    add penyesuaian gaji, fix bugs utk update status karyawan 2

--- resources/views/livewire/salary-adjustment.blade.php
+++ resources/views/livewire/salary-adjustment.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="d-flex  align-items-center py-2">
+    <div class="d-flex flex-column-reverse flex-lg-row align-items-center pt-2">
         <div class="col-12 col-lg-2">
             <select class="form-select" wire:model.live="pilihLamaKerja">
                 <option value=" ">{{ __('Pilih lama bekerja') }}</option>
@@ -10,44 +10,52 @@
                 <option value="210">{{ __('7 Bulan') }}</option>
               </select>
         </div>
-        <h4 class="mx-auto">{{ __('Penyesuaian Gaji') }}</h4>
+        <h3 class="mx-auto">{{ __('Penyesuaian Gaji') }}</h3>
        
     </div>
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                <th>{{ __('ID') }}</th>
-                <th>{{ __('Nama') }}</th>
-                <th>{{ __('Company') }}</th>
-                <th>{{ __('Departemen') }}</th>
-                <th>{{ __('Jabatan') }}</th>
-                <th>{{ __('Status') }}</th>
-                <th>{{ __('Tanggal Bergabung') }}</th>
-                <th>{{ __('Lama Bekerja') }}</th>
-                <th>{{ __('Gaji Pokok') }}</th>
-                <th>{{ __('Gaji Rekomendasi') }} : Rp {{ number_format($gaji_rekomendasi) }}</th>
-            </tr>
-        </thead>
-        <tbody>
-
-            @foreach ($data as $d)
+    <div class="table-responsive p-3">
+        <table class="table table-hover">
+            <thead>
                 <tr>
-                    <td>{{ $d->id_karyawan }}</td>
-                    <td>{{ $d->nama }}</td>
-                    <td>{{ $d->company }}</td>
-                    <td>{{ $d->departemen }}</td>
-                    <td>{{ $d->jabatan }}</td>
-                    <td>{{ $d->status_karyawan }}</td>
-                    <td>{{ $d->tanggal_bergabung }}</td>
-
-                    <td>{{ number_format(lama_bekerja($d->tanggal_bergabung, $today)) }}</td>
-                    <td>{{ number_format($d->gaji_pokok) }}</td>
-                    <td class="text-center">
-                        <button wire:click="sesuaikan(`{{ $d->id }}`)" class="btn btn-primary btn-sm">Sesuaikan</button>
-                    </td>
+                    <th>{{ __('ID') }}</th>
+                    <th>{{ __('Nama') }}</th>
+                    <th>{{ __('Company') }}</th>
+                    <th>{{ __('Departemen') }}</th>
+                    <th>{{ __('Jabatan') }}</th>
+                    <th>{{ __('Status') }}</th>
+                    <th>{{ __('Tanggal Bergabung') }}</th>
+                    <th>{{ __('Lama Bekerja') }}</th>
+                    <th>{{ __('Gaji Pokok') }}</th>
+                    <th>{{ __('Gaji Rekomendasi') }} : Rp {{ number_format($gaji_rekomendasi) }}</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-    {{ $data->links() }}
+            </thead>
+            <tbody>
+    
+                @foreach ($data as $d)
+                    <tr>
+                        <td>{{ $d->id_karyawan }}</td>
+                        <td>{{ $d->nama }}</td>
+                        <td>{{ $d->company }}</td>
+                        <td>{{ $d->departemen }}</td>
+                        <td>{{ $d->jabatan }}</td>
+                        <td>{{ $d->status_karyawan }}</td>
+                        <td>{{ $d->tanggal_bergabung }}</td>
+    
+                        <td>{{ number_format(lama_bekerja($d->tanggal_bergabung, $today)) }}</td>
+                        <td>{{ number_format($d->gaji_pokok) }}</td>
+                        <td class="text-center">
+                            <button wire:click="sesuaikan(`{{ $d->id }}`)" class="btn btn-primary btn-sm">Sesuaikan</button>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        {{ $data->links() }}
+    </div>
+    <style>
+        td,
+        th {
+            white-space: nowrap;
+        }
+    </style>
 </div>
