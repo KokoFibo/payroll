8228d2c3dd566d909e992348e118e67d85d72512
KokoFibo <kokonacci@gmail.com>
2024-01-31 00:12:50 +0700

    penambahan update contact darurat di menu user profile

--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -136,7 +136,7 @@
                         {{-- <label class="block text-sm font-medium  text-gray-900">Email Baru</label> --}}
                         <div class="relative mt-1 rounded-md shadow-sm mb-2">
                             <input type="text" wire:model="kontak_darurat"
-                                placeholder="{{ __('Nama yang dapat dihubungi saat darurat') }}"
+                                placeholder="{{ __('Nama kontak darurat') }}"
                                 class="block w-full rounded-md border-0 py-1.5 pl-3 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:">
                             @error('kontak_darurat')
                                 <div class="text-red-500">
