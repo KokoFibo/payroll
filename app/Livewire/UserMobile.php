ba606e4d7c5f6c7bf5a0a0529f9a61db7bd5a125
KokoFibo <kokonacci@gmail.com>
2024-02-04 11:17:18 +0700

    hilangkan jam kerja dan jam lembur jika tidak ada gaji lembur  1

--- app/Livewire/UserMobile.php
+++ app/Livewire/UserMobile.php
@@ -109,8 +109,8 @@ class UserMobile extends Component
         $this->cx++;
         // $this->user_id = 103;
         // $this->user_id = 1008;
-        $this->user_id = 1070;
-        // $this->user_id = auth()->user()->username;
+        // $this->user_id = 1070;
+        $this->user_id = auth()->user()->username;
         // $selectedMonth = 11;
 
         $total_hari_kerja = 0;
