9bcb9bb7651a72b98a1599ab903e40815369f119
KokoFibo <kokonacci@gmail.com>
2024-02-06 21:12:14 +0700

    benerin tampilan kerja minggu utk yg mingguan 1

--- app/Livewire/UserMobile.php
+++ app/Livewire/UserMobile.php
@@ -127,8 +127,8 @@ class UserMobile extends Component
         $this->cx++;
         // $this->user_id = 103;
         // $this->user_id = 1008;
-        $this->user_id = 1082;
-        // $this->user_id = auth()->user()->username;
+        // $this->user_id = 1082;
+        $this->user_id = auth()->user()->username;
         // $selectedMonth = 11;
 
         $total_hari_kerja = 0;
