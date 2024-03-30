b1eb83e9a640a15deecf8fd64ebbcb7fe2413665
KokoFibo <kokonacci@gmail.com>
2024-03-30 19:02:45 +0700

    rubah jam lembur jadi jam kerja untuk bulanan tambahan jabatan translator 3

--- app/helper2.php
+++ app/helper2.php
@@ -166,7 +166,7 @@ function build_payroll($month, $year)
                 //     $jam_lembur *= 2;
                 // }
                 if (
-                    is_libur_nasional($d->date) &&  !is_sunday($d->date) && $d->karyawan->jabatan != 'Translator' && $d->karyawan->etnis != 'Tionghoa'
+                    is_libur_nasional($d->date) &&  !is_sunday($d->date)
                 ) {
                     $jam_kerja *= 2;
                     $jam_lembur *= 2;
@@ -177,6 +177,11 @@ function build_payroll($month, $year)
                 if ((is_sunday($d->date) || is_libur_nasional($d->date)) && trim($d->karyawan->metode_penggajian) == 'Perbulan') {
                     $total_hari_kerja--;
                 }
+
+                // if (is_libur_nasional($d->date) &&  !is_sunday($d->date) && ($d->karyawan->jabatan == 'Translator' || $d->karyawan->etnis == 'Tionghoa')) {
+                //     $jam_kerja = 0;
+                //     $jam_lembur = 0;
+                // }
                 $total_jam_kerja = $total_jam_kerja + $jam_kerja;
                 $total_jam_lembur = $total_jam_lembur + $jam_lembur;
                 $total_keterlambatan = $total_keterlambatan + $terlambat;
