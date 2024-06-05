a7909e6b427defbfae6057f7464f7a2161cb8914
KokoFibo <kokonacci@gmail.com>
2024-06-05 19:40:12 +0700

    perbesar modal di detail payrol

--- resources/views/modals/payroll-modal.blade.php
+++ resources/views/modals/payroll-modal.blade.php
@@ -1,7 +1,7 @@
 <!-- Modal -->
 <div wire:ignore.self class="modal fade" id="payroll" tabindex="-1" aria-labelledby="exampleModalLabel"
     aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog  modal-lg">
         <div class="modal-content">
             <div class="modal-header">
                 <h1 class="modal-title fs-5" id="exampleModalLabel">Slip Gaji</h1>
