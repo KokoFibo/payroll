4186f0d29355a1d53a648fcc9d4db0868cfa1a7c
KokoFibo <kokonacci@gmail.com>
2024-06-25 14:58:00 +0700

    tambah pph21 utk bod

--- app/Livewire/Timeoffwr.php
+++ app/Livewire/Timeoffwr.php
@@ -6,6 +6,12 @@ use Livewire\Component;
 
 class Timeoffwr extends Component
 {
+    public function save()
+    {
+        dd('save');
+    }
+    public $id_karyawan, $placement, $type, $description, $start_date, $end_date;
+
     public function render()
     {
         return view('livewire.timeoffwr')->layout('layouts.polos');
