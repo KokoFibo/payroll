b0d4078a532ad08b65ef7358323781178b89e3e7
KokoFibo <kokonacci@gmail.com>
2024-07-17 08:35:28 +0700

    hide button cuti

--- resources/views/livewire/timeoffwr.blade.php
+++ resources/views/livewire/timeoffwr.blade.php
@@ -60,7 +60,8 @@
                     @if (!$is_add && !$is_update)
 
                         <div class='pl-3 pt-2 text-center'>
-                            <button wire:click='add' class='bg-indigo-500 text-white px-3 py-1 rounded-lg text-sm'>Buat
+                            <button hidden wire:click='add'
+                                class='bg-indigo-500 text-white px-3 py-1 rounded-lg text-sm'>Buat
                                 Permohonan
                                 Ijin/Cuti</button>
 
