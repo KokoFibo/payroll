0c236283f78713e9fe28f7e74a6e57d9e475c9ca
KokoFibo <kokonacci@gmail.com>
2024-09-19 09:09:37 +0700

    rubah lagi

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Karyawan;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
@@ -10,5 +11,21 @@ class ApiController extends Controller
     public function index()
     {
         return Karyawan::where('id_karyawan', '2')->get();
+    }
+
+    public function getDataUser($id)
+    {
+        // Find the user by ID
+        $user = User::where('username', $id)->first();
+
+        // Check if the user exists
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found'
+            ], 404);
+        }
+
+        // Return user data
+        return response()->json($user, 200);
     }
 }
