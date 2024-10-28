f0082ebee535a59ebc00e09891f31d78fb734aac
KokoFibo <kokonacci@gmail.com>
2024-10-28 11:23:59 +0700

    fix test

--- app/Livewire/Test.php
+++ app/Livewire/Test.php
@@ -1,5 +1,71 @@
-<div>
-  <h3>Change Placement</h3>
-  <button class="btn btn-primary" wire:click='proses'>Proses</button>
+<?php
 
-</div>
+namespace App\Livewire;
+
+use Carbon\Carbon;
+use App\Models\Ter;
+use App\Models\User;
+use App\Models\Payroll;
+use Livewire\Component;
+use App\Models\Karyawan;
+use App\Models\Tambahan;
+use App\Models\Jamkerjaid;
+use Livewire\WithPagination;
+use App\Models\Bonuspotongan;
+use App\Models\Department;
+use App\Models\Liburnasional;
+use App\Models\Personnelrequestform;
+use App\Models\Placement;
+use App\Models\Rekapbackup;
+use App\Models\Requester;
+use App\Models\Yfrekappresensi;
+use Illuminate\Support\Facades\DB;
+
+class Test extends Component
+{
+  // public $saturday;
+  use WithPagination;
+  protected $paginationTheme = 'bootstrap';
+  public $month;
+  public $year;
+  public $today;
+  public $cx;
+
+
+
+  public function mount()
+  {
+    $this->cx = 0;
+    $this->today = now();
+
+    $this->year = now()->year;
+    $this->month = now()->month;
+  }
+
+  public $department_id, $placement_id;
+  // public $karyawan;
+
+  public function proses()
+  {
+    Karyawan::where('placement_id', 6)->whereIn('department_id', [5, 11, 10, 2, 6])
+      ->update(['placement_id' => 102]);
+
+    $this->dispatch(
+      'message',
+      type: 'success',
+      title: 'Placement Berhasil dirubah',
+    );
+  }
+
+  public function render()
+  {
+
+
+
+
+
+
+
+    return view('livewire.test');
+  }
+}
--- resources/views/livewire/test.blade.php
+++ resources/views/livewire/test.blade.php
@@ -1,8 +1,5 @@
-<div class='col-3 m-5'>
+<div>
+    <h3>Change Placement</h3>
+    <button class="btn btn-primary" wire:click='proses'>Proses</button>
 
-    <div class="mb-3">
-        <label class="form-label">Karyawan ID</label>
-        <input wire:model='karyawan_id' class="form-control">
-    </div>
-    <button wire:click='changePresensiId' class="btn btn-primary mt-3">Rubah</button>
 </div>
