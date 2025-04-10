5da5d5542f02fb45dcf92cc2bded99a86c1b0158
KokoFibo <kokonacci@gmail.com>
2025-04-10 10:57:59 +0700

    gak bisa upload

--- app/Rules/AllowedFileExtension.php
+++ app/Rules/AllowedFileExtension.php
@@ -3,6 +3,7 @@
 namespace App\Rules;
 
 use Closure;
+use Illuminate\Support\Str;
 use Illuminate\Contracts\Validation\ValidationRule;
 
 class AllowedFileExtension implements ValidationRule
@@ -14,6 +15,15 @@ class AllowedFileExtension implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
+
+        \Log::info('File info', [
+            'class' => get_class($value),
+            'originalName' => $value?->getClientOriginalName(),
+            'mimeType' => $value?->getMimeType(),
+            'realMimeType' => $value?->getPathname() ? mime_content_type($value->getPathname()) : null,
+            'extension' => $value?->getClientOriginalExtension(),
+            'exists' => $value && file_exists($value->getPathname()),
+        ]);
         if (!$value instanceof \Illuminate\Http\UploadedFile) {
             $fail('File tidak valid.');
             return;
