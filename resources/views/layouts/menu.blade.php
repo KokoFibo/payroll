ebe46a397f4794e8195e7cf358e581ba50866241
KokoFibo <kokonacci@gmail.com>
2025-05-28 11:33:07 +0700

    tambah report detail

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -230,6 +230,12 @@ with font-awesome or any other icon font library -->
 
 
             @if (Auth::user()->role > 7)
+                <li class="nav-item {{ 'data-log' == request()->path() ? 'bg-secondary rounded' : '' }}">
+                    <a href="/laporan" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>{{ __('Laporan') }}</p>
+                    </a>
+                </li>
                 <li class="nav-item {{ 'applicantditerima' == request()->path() ? 'bg-secondary rounded' : '' }}">
                     <a href="/applicantditerima" class="nav-link">
                         <i class="far fa-circle nav-icon"></i>
