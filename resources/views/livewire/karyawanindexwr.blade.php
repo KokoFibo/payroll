e42c79be0de7b2df38401a10e59196e9b1e3cf14
KokoFibo <kokonacci@gmail.com>
2025-06-12 09:35:50 +0700

    excel template form placement only

--- resources/views/livewire/karyawanindexwr.blade.php
+++ resources/views/livewire/karyawanindexwr.blade.php
@@ -197,7 +197,11 @@
 
                                     <th style="width: 150px; border-style: none;">
                                         <a href="/template-gaji-form"> <button wire:loading.remove
-                                                class="btn btn-success col-12">Excel Template</button></a>
+                                                class="btn btn-success col-12">Excel Template Department</button></a>
+                                    </th>
+                                    <th style="width: 150px; border-style: none;">
+                                        <a href="/template-gaji-form-placement"> <button wire:loading.remove
+                                                class="btn btn-success col-12">Excel Template Placement</button></a>
                                     </th>
 
                                     @if (auth()->user()->role == 8)
