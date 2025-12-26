1af022c392c144f064064d46f6cc05c6d2247b75
KokoFibo <kokonacci@gmail.com>
2025-12-26 08:11:07 +0700

    google tag2

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -120,7 +120,18 @@
         </script>
 
     </div>
+    <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-8KWREPG05Q"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-8KWREPG05Q');
+    </script>
     <script>
         window.dataLayer = window.dataLayer || [];
 
