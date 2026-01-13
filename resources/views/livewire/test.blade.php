73d62405824b99bd2263d0990ec66b6b8d2f10ae
KokoFibo <kokonacci@gmail.com>
2026-01-13 09:35:40 +0700

    turnover

--- resources/views/livewire/test.blade.php
+++ resources/views/livewire/test.blade.php
@@ -1,27 +1,4 @@
 <div class="max-w-4xl mx-auto p-4">
-    <h1>Non OS yang jam kerja dibawah 8 jam dan gaji hariannya tidak tercatat</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nama</th>
-                <th>Total Jam Kerja</th>
-                <th>Total hari kerja</th>
-                <th>Tanggal</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($data as $key => $d)
-                <tr>
-                    <td>{{ $d->user_id }}</td>
-                    <td>{{ $d->nama }}</td>
-                    <td>{{ $d->total_jam_kerja }}</td>
-                    <td>{{ $d->total_hari_kerja }}</td>
-                    <td>{{ $d->date }}</td>
-                    {{-- <td>{{ $d->total_jam_kerja_libur }}</td> --}}
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-    <h1>total = {{ $total }}</h1>
+    <livewire:turnover />
+
 </div>
