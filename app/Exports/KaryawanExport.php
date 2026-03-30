8e056f783df50317521808258d442c312222b335
KokoFibo <kokonacci@gmail.com>
2026-03-30 17:06:49 +0700

    tambah data agama di karyawan

--- app/Exports/KaryawanExport.php
+++ app/Exports/KaryawanExport.php
@@ -32,12 +32,18 @@ class KaryawanExport implements FromView,  ShouldAutoSize, WithColumnFormatting,
     {
 
         if ($this->selectStatus == 1) {
-            $statuses = ['PKWT', 'PKWTT', 'Dirumahkan'];
+            $statuses = ['PKWT', 'PKWTT'];
         } elseif ($this->selectStatus == 2) {
-            $statuses = ['Blacklist', 'Resigned'];
+            $statuses = ['Resigned'];
+        } elseif ($this->selectStatus == 3) {
+            $statuses = ['Blacklist'];
+        } elseif ($this->selectStatus == 4) {
+            $statuses = ['Dirumahkan'];
         } else {
             $statuses = ['PKWT', 'PKWTT', 'Dirumahkan', 'Resigned', 'Blacklist'];
         }
+
+
         $data = Karyawan::whereIn('status_karyawan', $statuses);
 
         if ($this->selected_placement) {
