2042aafcc4a14ea28774bca923e75bf7e2f75643
KokoFibo <kokonacci@gmail.com>
2026-04-16 13:25:37 +0700

    is locked

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -83,6 +83,10 @@ class AttendanceController extends Controller
             ->values()
             ->toArray();
 
+        // is slip gaji locked
+        $is_locked = Lock::value('slip_gaji');
+
+
         // Jika tidak ada data attendance
         if ($attendanceData->isEmpty()) {
             return response()->json([
@@ -90,6 +94,7 @@ class AttendanceController extends Controller
                 'data' => [],
                 'available_months' => $availableMonths,
                 'summary' => $this->getEmptySummary(),
+                'is_locked' => $is_locked,
                 'current_month_year' => [
                     'month' => (int)$month,
                     'year' => (int)$year,
@@ -110,6 +115,7 @@ class AttendanceController extends Controller
             'data' => $transformedData,
             'available_months' => $availableMonths,
             'summary' => $summary,
+            'is_locked' => $is_locked,
             'current_month_year' => [
                 'month' => (int)$month,
                 'year' => (int)$year,
