279b712e7abf80ab8ac9156857b4689538d7b1ca
KokoFibo <kokonacci@gmail.com>
2026-09-19 11:15:20 +0700

    tambah isi dashboard2

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -69,7 +69,7 @@
     {{-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> --}}
 
     {{-- @livewireStyles --}}
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.min.js"></script>
+    {{-- <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.min.js"></script> --}}
 </head>
 <style>
     body {
